080d8f982c7c8812b5e54ef41e8062783f2f92d6
Blaise Wang <blaise.ywang@gmail.com>
2024-08-27 02:05:29 -0700

    Footer & Header Updated

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,12 +2,26 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}', "./node_modules/flowbite/**/*.js"],
 	theme: {
-		
+		borderOpacity: ({ theme }) => theme('opacity'),
+    borderRadius: {
+      none: '0px',
+      sm: '0.125rem',
+      DEFAULT: '0.25rem',
+      md: '0.375rem',
+      lg: '0.5rem',
+      xl: '0.75rem',
+      '2xl': '1rem',
+      '3xl': '1.5rem',
+	  '1/6': '16%',
+	  '1/5': '20%',
+	  '1/3': '33%',
+      full: '9999px',
+    	},
 		extend: {},
 	},
-	plugins: [
-		require('flowbite/plugin')
-	],
 
+	plugins: [
+		require('flowbite/plugin'),
+	],
 }
 
